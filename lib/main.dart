6e95763df706e68e0aa1961edad35d4d04d290e4
Slavi Traykov <st@auxcode.com>
2022-03-02 11:40:16 +0200

    removed template code

--- lib/main.dart
+++ lib/main.dart
@@ -39,31 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const HeaderBar(
+    return const Scaffold(
+      appBar: HeaderBar(
         title: 'Technische Hotline',
         phone: '0800 123 456 78',
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      )
     );
   }
 }
