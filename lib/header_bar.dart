624b1c397741b82628ee09c3dc14fd28b7bcf46d
Slavi Traykov <st@auxcode.com>
2022-03-04 16:18:19 +0200

    header_bar.dart fixes

--- lib/header_bar.dart
+++ lib/header_bar.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const titleStyle = TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 14);
+
+const phoneStyle = TextStyle(fontWeight: FontWeight.normal, color: Colors.white, fontSize: 18);
+
 const backgroundColor = Color(0xFF00282D);
 const borderColor = Color(0xFF005661);
+const screenWidthValue = 767;
+const containerDecoration =
+    BoxDecoration(color: backgroundColor, border: Border(bottom: BorderSide(width: 1, color: borderColor)));
 
+const logoPadding = EdgeInsets.only(top: 9, bottom: 9, left: 0, right: 0);
+
+const iconPadding = EdgeInsets.only(top: 9, right: 15, bottom: 9, left: 0);
+
+const textPadding = EdgeInsets.only(top: 10, bottom: 6, left: 0, right: 0);
+
+const logoWidth = 140.00;
+
+const iconWidth = 42.00;
+
+const containerHeight = 58.00;
 class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final String phone;
 
-  double getScreenWidth(BuildContext context) {
-    return MediaQuery.of(context).size.width;
-  }
+  double getScreenWidth(BuildContext context) => MediaQuery.of(context).size.width;
 
-  double getPadding(BuildContext context) {
-    if (getScreenWidth(context) > 767) {
-      return 43;
-    } else {
-      return 0;
-    }
+  EdgeInsets getPadding(BuildContext context) {
+    var padding = getScreenWidth(context) > screenWidthValue ? 43.00 : 0.00;
+    return EdgeInsets.only(left: 14, right: padding);
   }
 
   const HeaderBar({
@@ -29,51 +42,45 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          color: backgroundColor,
-          border: Border(bottom: BorderSide(width: 1, color: borderColor))),
-      height: 58,
-      padding: EdgeInsets.only(left: 14, right: getPadding(context)),
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-              padding: const EdgeInsets.only(top: 9, bottom: 9),
-              child: Image.asset(
-                'logo.png',
-                width: 140,
-              )),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 600, top: 9, right: 15, bottom: 9),
-            child: Image.asset(
-              'icon.png',
-              width: 42,
+        decoration: containerDecoration,
+        height: containerHeight,
+        padding: getPadding(context),
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: logoPadding,
+                    child: Image.asset(
+                      'logo.png',
+                      width: logoWidth,
+                    ))
+              ],
             ),
-          ),
-          if (getScreenWidth(context) > 767)
-            Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 6),
-                child: Column(
-                  children: [
-                    Text(title,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 14)),
-                    Text(phone,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.white,
-                            fontSize: 18))
-                  ],
-                )),
-        ],
-      ),
-    );
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: iconPadding,
+                  child: Image.asset(
+                    'icon.png',
+                    width: iconWidth,
+                  ),
+                ),
+                if (getScreenWidth(context) > screenWidthValue)
+                  Padding(
+                      padding: textPadding,
+                      child: Column(
+                        children: [Text(title, style: titleStyle), Text(phone, style: phoneStyle)],
+                      )),
+              ],
+            )
+          ],
+        ));
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(58);
+  Size get preferredSize => const Size.fromHeight(containerHeight);
 }
